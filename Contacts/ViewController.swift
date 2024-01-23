392b2bb15ff54a10e57953057a8b2236a85d0d18
jaegc <jaeleegcollins@outlook.com>
2024-01-23 13:29:41 -0500

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Jae Collins on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
